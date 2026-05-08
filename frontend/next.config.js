85704cb5799fdb1ec2dd0a6522b3b3f4ac4c83ff
Conetwork <nachohan12@gmail.com>
2026-05-08 15:29:52 +0500

    fix api url

--- frontend/next.config.js
+++ frontend/next.config.js
@@ -3,8 +3,7 @@ const nextConfig = {
   reactStrictMode: true,
   swcMinify: true,
   env: {
-    NEXT_PUBLIC_API_URL: process.env.NEXT_PUBLIC_API_URL || 'http://localhost:5000',
+    NEXT_PUBLIC_API_URL: 'https://conetwork-booking-platform.onrender.com',
   },
 }
-
 module.exports = nextConfig
